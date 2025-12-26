baf50f96437c71e18c629639d02481b10edd0443
Aleksey Kuznetsov <avkuznetsov@mmip.ru>
2025-12-26 20:08:37 +0300

    Add new get responce function

--- functions.py
+++ functions.py
@@ -2,7 +2,53 @@ import requests
 import random
 import re
 from config import male_names, female_names
-#from transliterate import translit
+import base64
+
+
+def get_wp_nonce2():
+
+    # 1. Define the URL
+    url = "https://ylilit.ru"
+
+    str_encoded = "Ci8qIDwhW0NEQVRBWyAqLwp2YXIgZGF0YUFqYXggPSB7InVybCI6Imh0dHBzOi8veWxpbGl0LnJ1L3dwLWFkbWluL2FkbWluL" \
+                  "WFqYXgucGhwIiwibm9uY2UiOiIyOTIyZjZmMGVhIn07Ci8vIyBzb3VyY2VVUkw9Y2FydC1qcy1leHRyYQovKiBdXT4gKi8K"
+
+    # 2. Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # 3. Check if the request was successful (optional but recommended)
+    if response.status_code == 200:
+        # 4. Access the page content as a string (decoded Unicode)
+        page_content_text = response.text
+
+        for i in page_content_text.split(" "):
+            if str_encoded in i:
+                pattern = r'src="data:text/javascript;base64,([^"]+)"></script>'
+                match = re.search(pattern, i)
+                if match:
+                    base64_str = match.group(1)
+
+                    decoded_bytes = base64.b64decode(base64_str)
+                    decoded_text = decoded_bytes.decode('utf-8')
+                    #print(decoded_text)
+
+                    decoded_text2 = decoded_text.replace('/* <![CDATA[ */','')
+
+                    decoded_text2 = decoded_text2.replace('//# sourceURL=cart-js-extra','')
+
+                    decoded_text2 = decoded_text2.replace('/* ]]> */','')
+
+                    decoded_text2 = decoded_text2.strip()
+
+                    #print(decoded_text2)
+
+                    pattern2 = r'var dataAjax = {"url":"https://ylilit.ru/wp-admin/admin-ajax.php","nonce":"(.*)"};'
+                    match2 = re.search(pattern2, decoded_text2)
+                    if match2:
+                        nonce = match2.group(1)
+
+                        return nonce
+
 
 def get_wp_nonce():
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import random
 from config import headers, my_cookies, url
-from functions import get_wp_nonce, generate_phone_number, make_menu, generate_random_email
+from functions import get_wp_nonce2, generate_phone_number, make_menu, generate_random_email
 from functions import random_russian_name, get_random_yaroslavl_address
 import time
 
@@ -18,9 +18,9 @@ def run_task(wp_nonce_code):
     # Get new random dish
     dish_id = random.choice(list(dishes.keys()))
     dish_name = dishes[dish_id][0]
-    dish_value = dishes[dish_id][1]
+    dish_value = int(dishes[dish_id][1])
     dish_img = dishes[dish_id][2]
-    dish_price = dishes[dish_id][3]
+    dish_price = int(dishes[dish_id][3])
 
     cart = {
         f"cart[{dish_id}]":
@@ -32,11 +32,11 @@ def run_task(wp_nonce_code):
     }
 
     try:
-        cart_json = json.dumps(cart).encode('utf8')
+        cart_json = json.dumps(cart,ensure_ascii=False)
     except:
         cart = {"cart[4637]": [3, "https://ylilit.ru/wp-content/uploads/2023/11/Kebab-v-lavashe.webp", "300",
                         "Кебаб с тертым сыром в лаваше .(Фирменное блюдо)", "385"]}
-        cart_json = json.dumps(cart).encode('utf8')
+        cart_json = json.dumps(cart,ensure_ascii=False)
 
     # Get random name
     name = random_russian_name()
@@ -59,10 +59,12 @@ def run_task(wp_nonce_code):
     }
 
     print(name, phone, email, address, dish_name, dish_value, dish_price, nonce_code)
+    print(data)
+
 
     # Make request and get responce
     try:
-        responce = requests.post(url, headers=headers, cookies=my_cookies, data=data, timeout=10)
+        responce = requests.post(url, headers=headers, cookies=my_cookies, data=data, timeout=20)
     except:
         print("Connection error...")
     else:
@@ -71,13 +73,14 @@ def run_task(wp_nonce_code):
 
 count = 0
 #nonce_code = get_wp_nonce()
+#nonce_code = "2922f6f0ea"
 
 while True:
     print(f"Task: {count}")
-    if count % 10 == 0:
-        nonce_code = get_wp_nonce()
+    if count % 30 == 0:
+        nonce_code = get_wp_nonce2()
         print("Code updated!")
     run_task(nonce_code)
     count+=1
-    time.sleep(10)
+    time.sleep(20)
 
