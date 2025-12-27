9cfeebecbb211d8b54f255a9fd8abc8129bd7636
Aleksey Kuznetsov <avkuznetsov@mmip.ru>
2025-12-27 16:36:55 +0300

    Refactoring

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def run_task(wp_nonce_code):
     # Get random address
     #address = get_random_yaroslavl_address()
 
-    address = "Извинитесь перед учителем, которого вы оскорбили и все прекратится."
+    address = "Извинитесь перед учителем и другими людьми, которорых вы оскорбили и все прекратится."
 
     # Form new order
     data = {
